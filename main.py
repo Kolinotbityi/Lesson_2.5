e6091b213125ef6094ccf4f3d3e9495320860ef7
Алеша <98235139+Kolinotbityi@users.noreply.github.com>
2024-09-25 19:39:59 +0400

    Update main.py

--- main.py
+++ main.py
@@ -6,11 +6,14 @@ def get_matrix(n, m, valve):
         list_ = []
         matrix.append(list_)
         for j in range(m) :
-            list_.append(valve)
+            if valve > 0 :
+                list_.append(valve)
+            else :
+                list_ = []
     return matrix
 
 
-result1 = get_matrix(2, 2, 10)
+result1 = get_matrix(2, 2, 0)
 result2 = get_matrix(3, 5, 42)
 result3 = get_matrix(4, 2, 13)
 print(result1)
